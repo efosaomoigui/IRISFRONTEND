25d918c2fedd1e0d71da96f76be313756bee9a5b
Valentine Madu <valentine@chiscoexpress.com>
2022-02-06 14:58:57 +0100

    update private route

--- src/app/modules/shipmentmanagement/components/settings/ViewRoutes.tsx
+++ src/app/modules/shipmentmanagement/components/settings/ViewRoutes.tsx
@@ -6,7 +6,7 @@ import { TablesWidgetRoute } from "./TableWidgetRoute";
 
 
 export function ViewRoutes() {
-    const [routemodels, setRouteModel] = useState<IRouteModel[]>([]);
+    const [routemodel1, setRouteModel] = useState<IRouteModel[]>([]);
 
     // //USE EFFECT HOOK
     useEffect(() => {
@@ -28,7 +28,7 @@ export function ViewRoutes() {
     return (
         <div className='row g-5 g-xxl-8'>
             <div className='col-xl-12'>
-                <TablesWidgetRoute className='mb-5 mb-xxl-8' route={routemodels} />
+                <TablesWidgetRoute className='mb-5 mb-xxl-8' route={routemodel1} />
             </div>
         </div>
     )
