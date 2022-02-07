c54964cead6fc79dda628579d7ee8e263d1677d7
Valentine Madu <valentine@chiscoexpress.com>
2022-02-07 17:02:34 +0100

    add forms for permission and role

--- src/app/modules/usermanagement/userformwidget/AddPermissionForm.tsx
+++ src/app/modules/usermanagement/userformwidget/AddPermissionForm.tsx
@@ -5,31 +5,21 @@ const AddPermissionForm: React.FC = () => {
   return (
     <>
       <Form>
-        <Row>
+        
           <Form.Group as={Col} controlId='formGridEmail'>
-            <Form.Label>Email</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' />
+            <Form.Label>Id</Form.Label>
+            <Form.Control placeholder='Enter Id' />
           </Form.Group>
 
           <Form.Group as={Col} controlId='formGridPassword'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' />
+            <Form.Label>RoleId</Form.Label>
+            <Form.Control placeholder='Enter role id' />
           </Form.Group>
-        </Row>
-
-        <Form.Group controlId='formGridAddress1'>
-          <Form.Label>Address</Form.Label>
-          <Form.Control placeholder='1234 Main St' />
-        </Form.Group>
-
-        <Form.Group controlId='formGridAddress2'>
-          <Form.Label>Phone No.</Form.Label>
-          <Form.Control placeholder='Enter Phone Number' />
-        </Form.Group>
+      
 
         <Row>
           <Form.Group as={Col} controlId='formGridState'>
-            <Form.Label>City</Form.Label>
+            <Form.Label>Claim type</Form.Label>
             <Form.Control as='select'>
               <option>Choose...</option>
               <option>...</option>
@@ -37,22 +27,13 @@ const AddPermissionForm: React.FC = () => {
           </Form.Group>
 
           <Form.Group as={Col} controlId='formGridState'>
-            <Form.Label>State</Form.Label>
+            <Form.Label>Claim Value</Form.Label>
             <Form.Control as='select'>
               <option>Choose...</option>
               <option>...</option>
             </Form.Control>
           </Form.Group>
-
-          <Form.Group as={Col} controlId='formGridZip'>
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
-          </Form.Group>
         </Row>
-
-        <Form.Group id='formGridCheckbox'>
-          <Form.Check type='checkbox' label='Check me out' />
-        </Form.Group>
 
         <Button variant='primary' type='submit'>Submit</Button>
       </Form>
