f6150eb77a3c7b6048673acfdee226ab7a1f37ba
Omoigui Efosa <efe.omoigui@gmail.com>
2022-02-15 06:41:28 +0100

    Add toaster to submit

--- src/app/modules/usermanagement/modals/AddUserModal.tsx
+++ src/app/modules/usermanagement/modals/AddUserModal.tsx
@@ -1,29 +1,31 @@
-import { useState } from 'react';
-import { v4 as uuid } from 'uuid';
-import agent from '../../../../setup/axios/AxiosAgent';
-import { IUserModel } from '../../auth/models/AuthInterfaces';
-import AddUserForm from '../userformwidget/AddUserForm';
-
+import {useState} from 'react'
+import { toast } from 'react-toastify'
+import {v4 as uuid} from 'uuid'
+import agent from '../../../../setup/axios/AxiosAgent'
+import {IUserModel} from '../../auth/models/AuthInterfaces'
+import AddUserForm from '../userformwidget/AddUserForm'
 
 const AddUserModal: React.FC = () => {
-
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
   const onSubmit = (values: IUserModel) => {
-    setIsSubmitting(true);
-    values.userId = uuid();
-    agent.Users.create(values).then((response) =>{ console.log(response)});
+    setIsSubmitting(true)
+    values.userId = uuid()
+    agent.Users.create(values).then((response) => {
+      toast.success("User Creation Was Successful!");
+      console.log(response)
+      setIsSubmitting(false)
+    })
     // console.log("TT: ", values);
-  };
+  }
 
   return (
     <>
       <div className='modal fade' id='kt_modal_adduser' aria-hidden='true'>
-        <AddUserForm isSubmitting={isSubmitting} onSubmit={onSubmit}  />
+        <AddUserForm isSubmitting={isSubmitting} onSubmit={onSubmit} />
       </div>
     </>
   )
 }
 
-export { AddUserModal };
-
+export {AddUserModal}
