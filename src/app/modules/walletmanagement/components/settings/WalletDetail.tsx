09d6bfdf181ea877e5036f9c1b346ff7a958bbf9
Valentine Madu <valentine@chiscoexpress.com>
2022-02-22 07:52:54 +0100

    edit detail page

--- src/app/modules/walletmanagement/components/settings/WalletDetail.tsx
+++ src/app/modules/walletmanagement/components/settings/WalletDetail.tsx
@@ -24,18 +24,18 @@ export function WalletDetail() {
         <div className='card mb-5 mb-xl-10' id='kt_profile_details_view'>
           <div className='card-header cursor-pointer'>
             <div className='card-title m-0'>
-              <h3 className='fw-bolder m-0'>Profile Details</h3>
+              <h3 className='fw-bolder m-0'>Wallet Details</h3>
             </div>
 
             <Link to='/adminSettings/settings' className='btn btn-primary align-self-center'>
-              Edit Role
+              Edit Wallet
             </Link>
           </div>
 
           <div className='card-body p-9'>
             {walletdetails && <>
             <div className='row mb-7'>
-              <label className='col-lg-4 fw-bold text-muted'>Permission Name</label>
+              <label className='col-lg-4 fw-bold text-muted'>Wallet Name</label>
 
               <div className='col-lg-8'>
                 <span className='fw-bolder fs-6 text-dark'>
@@ -111,7 +111,7 @@ export function WalletDetail() {
             </div>
             </>}
 
-            {!walletdetails && <><h4>Sorry, Role does not exit!</h4></>}
+            {!walletdetails && <><h4>Sorry, Wallet does not exit!</h4></>}
 
           </div>
         </div>
